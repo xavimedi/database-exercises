18298f12d97a16e5953ac7c2b211373500651471
Xavier <xavier.g.medina3@gmail.com>
2023-07-12 10:02:03 -0500

    select exercises complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS albums(
     id int unsigned not null auto_increment,
     artist varchar(255),
     name varchar(255),
-    release_date char(4),
-    sales_in_millions decimal (4,4),
+    date char(4),
+    sales_in_M decimal (4,4),
     genre varchar(255),
     primary key (id) );
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,25 @@
 USE codeup_test_db;
+
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name
+FROM albums
+WHERE date BETWEEN 1990 AND 1999;
+
+SELECT name
+FROM albums
+WHERE sales_in_M < 20;
+
+SELECT name
+FROM albums
+WHERE genre = 'rock';
