49b57abc9ffd6d8dbb81cdb2cf234bb1f259ecbb
Xavier <xavier.g.medina3@gmail.com>
2023-07-13 09:57:40 -0500

    mySQL WHERE exercise complete, fixed a typo

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,7 +8,7 @@ select * from employees where last_name like '%q%';
 
 select * from employees where first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya') AND  gender = 'M';
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya') AND gender = 'M';
 
 select * from employees where last_name like 'E%' or last_name like '%E';
 
