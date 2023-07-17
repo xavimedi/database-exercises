4f6229518d26940631c0dfe304abb838b4460611
Xavier <xavier.g.medina3@gmail.com>
2023-07-17 13:43:27 -0500

    mySQL subqueries exercise complete3

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -24,8 +24,16 @@ where emp_no in (
     where first_name = 'Aamod'
 );
 
+select first_name, last_name
+from employees
+where gender = 'F' and emp_no in (
+    select emp_no
+    from dept_manager
+    where to_date like '9999%'
+    );
+
 select distinct e.first_name, e.last_name
-from employees as e
-join dept_manager as dm on e.emp_no = dm.emp_no
-join dept_emp as de on dm.dept_no = de.dept_no
-where e.gender = 'F' and dm.to_date = '9999-01-01';
+    from employees as e
+    join dept_manager as dm on e.emp_no = dm.emp_no
+    join dept_emp as de on dm.dept_no = de.dept_no
+    where e.gender = 'F' and dm.to_date = '9999-01-01';
